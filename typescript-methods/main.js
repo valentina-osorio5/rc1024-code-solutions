5a4f4364377fa3fa056c3a7b273bfbbac69a2210
valentina osorio <va.os.consulting@gmail.com>
2024-10-24 17:01:09 +0000

    almost finished typescript-methods

---
+++ typescript-methods/main.js
@@ -0,0 +1,30 @@
+'use strict';
+const heroes = ['mom', 'family', 'cousin', 'changemakers'];
+let randomNumber = Math.random();
+console.log('Math.random:', randomNumber);
+randomNumber = heroes.length * randomNumber;
+const randomIndex = Math.floor(randomNumber);
+console.log('randomIndex:', randomIndex);
+const randomHero = heroes[randomIndex];
+console.log('randomHero:', randomHero);
+const library = [
+  { title: 'Book 1', author: 'Author 1' },
+  { title: 'Book 2', author: 'Author 2' },
+  { title: 'Book 3', author: 'Author 3' },
+];
+const lastBook = library.pop();
+console.log('lastBook:', lastBook);
+const firstBook = library.shift();
+console.log('firstBook:', firstBook);
+const js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer',
+};
+const css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou',
+};
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+console.log('library', library);
