e92048d4b9a29bd6d241c1fa4be1287f1e818779
valentina osorio <va.os.consulting@gmail.com>
2024-11-20 01:33:34 +0000

    completed array-methods: Valentina Osorio

---
+++ array-methods/for-each.ts
@@ -0,0 +1,13 @@
+const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
+
+console.log('In order:');
+values.forEach((element) => console.log(element));
+
+// values.forEach(arrayFunction)9));
+console.log('Reverse order:');
+values.forEach((n, index) => console.log(values[values.length - 1 - index]));
+
+// values.forEach(function (n, index) {
+//   console.log("n", n); // current item in values array
+//   console.log("index: ", index);
+//   console.log(values[values.length - 1 - index])})
