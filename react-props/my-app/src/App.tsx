80429367765ae1a60a3976a462a9105a689cbe38
Valentina Osorio <va.os.consulting@gmail.com>
2024-12-10 17:01:10 -0500

    Update App.tsx

--- react-props/my-app/src/App.tsx
+++ react-props/my-app/src/App.tsx
@@ -6,8 +6,8 @@ import { Button } from './Button';
 import './App.css';
 
 const imageSRC = '/starry-sky.jpeg';
-const caption = 'A beautiful Image of Space';
-const description = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam incidunt
+const captionText = 'A beautiful Image of Space';
+const descriptionText = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam incidunt
       sit dolorum accusantium sapiente reiciendis maxime dolores ullam delectus,
       commodi placeat eveniet, quam voluptates facere et magnam architecto
       quaerat velit voluptas rerum quos asperiores quis in! Nemo aut aliquam
@@ -20,9 +20,9 @@ export default function App() {
   return (
     <>
       <Header headerText="React Image Bank" />
-      <Image image={imageSRC} />
-      <Caption captionText={caption} />
-      <Description descriptionText={description} />
+      <Image imageSRC={imageSRC} />
+      <Caption captionText={captionText} />
+      <Description descriptionText={descriptionText} />
       <Button buttonText="Click For Next Image" />
     </>
   );
