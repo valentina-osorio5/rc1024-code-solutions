db9f33acd7210a778e87ef73ac963f851e728479
valentina osorio <va.os.consulting@gmail.com>
2024-12-10 17:55:26 +0000

    completed react-components:Valentina Osorio

---
+++ react-components/my-app/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Header } from './Header';
+import { Image } from './Image';
+import { Caption } from './Caption';
+import { Description } from './Description';
+import { Button } from './Button';
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Image />
+      <Caption />
+      <Description />
+      <Button />
+    </>
+  );
+}
---
+++ react-components/my-app/src/Button.tsx
@@ -0,0 +1,3 @@
+export function Button() {
+  return <button type="button">Click for next image</button>;
+}
