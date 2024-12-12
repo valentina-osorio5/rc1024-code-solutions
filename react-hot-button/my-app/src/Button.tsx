e7f23abda2502a2ed2cacac85241700800d6e083
valentina osorio <va.os.consulting@gmail.com>
2024-12-12 01:17:34 +0000

    completed react-hot-button: Valentina Osorio

--- react-hot-button/my-app/src/Button.tsx
+++ react-hot-button/my-app/src/Button.tsx
@@ -9,38 +9,23 @@ type Props = {
 export function Button({ label }: Props) {
   const [count, setCount] = useState(0);
 
-  function handleClick() {
-    console.log('handleClick fired');
-    setCount(count + 1);
-    setColor();
+  console.log('handleClick fired');
 
-    function setColor() {
-      if (count <= 3) {
-        color = 'darkpurple';
-      }
-      if (count <= 6) {
-        color = 'lightpurple';
-      }
-      if (count <= 9) {
-        color = 'orange';
-      }
-      if (count <= 12) {
-        color = 'lightorange';
-      }
-      if (count <= 15) {
-        color = 'yellow';
-      }
-      if (count <= 18) {
-        color = 'white';
-      }
-      return color;
-    }
+  function setColor(): string {
+    if (count <= 3) return 'darkpurple';
+    if (count <= 6) return 'lightpurple';
+    if (count <= 9) return 'orange';
+    if (count <= 12) return 'lightorange';
+    if (count <= 15) return 'yellow';
+    return 'white';
   }
+
+  const color = setColor();
 
   return (
     <>
       <button
-        onClick={handleClick}
+        onClick={() => setCount(count + 1)}
         id="hot-button"
         className={`hot-button ${color}`}>
         {label}
