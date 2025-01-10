555797e2f200b84e14b7551592a05fa9401c0165
Valentina Osorio <va.os.consulting@gmail.com>
2025-01-10 17:28:48 +0000

    completed sql-aggregates:Valentina Osorio

--- sql-aggregates/movie-maniacs.sql
+++ sql-aggregates/movie-maniacs.sql
@@ -1,9 +1,8 @@
 -- this is not quite right
 
 select "c"."firstName", "c"."lastName","c"."customerId",
-        sum("amount") as "total paid"
+        sum("p"."amount") as "total paid"
 from "customers" as "c"
-join "rentals" using ("customerId")
 join "payments" as "p" using ("customerId")
 group by "c"."customerId"
-order by "p"."amount" desc;
+order by "total paid" desc;
